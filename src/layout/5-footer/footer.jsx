adcbf73cec088470d909213245ce5c39b85a10ea
SaToGo1 <stoledano97@gmail.com>
2023-11-22 20:58:34 +0100

    Change color variables

--- src/layout/5-footer/footer.jsx
+++ src/layout/5-footer/footer.jsx
@@ -3,8 +3,7 @@ import './footer.css'
 export function Footer () {
   return (
     <footer className='footer'>
-      <p>Santiago Toledano</p>
-      <p>Ⓒ 2022</p>
+      <p>Footer?</p>
     </footer>
   )
 }
