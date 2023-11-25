1a7d214597bb7ec4ad2f3c3ed21043f3461326b1
SaToGo1 <stoledano97@gmail.com>
2023-11-25 03:22:13 +0100

    change vite base page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portafolio-v1/',
+  base: 'https://SaToGo1.github.io/portfolio-v1'
 })
