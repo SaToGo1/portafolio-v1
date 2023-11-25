02d11c0cce1c80029a7706877d02923b552c4b37
SaToGo1 <stoledano97@gmail.com>
2023-11-25 03:48:44 +0100

    gh pages configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import VitePress from 'vitepress'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://SaToGo1.github.io/portfolio-v1'
+  base: '/portfolio-v1/',
+  plugins: [VitePress()],
 })
