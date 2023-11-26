69042bc30c84991b12385b1e167f2d5621a83f87
SaToGo1 <stoledano97@gmail.com>
2023-11-26 15:29:15 +0100

    Improve home section
    - Animations
    - shadow
    - padding
    - responsive

--- src/layout/1-home/home.jsx
+++ src/layout/1-home/home.jsx
@@ -1,14 +1,24 @@
 import './home.css'
 import { ExpandButton } from '../components/expand-button/expand-button'
+import { useEffect } from 'react'
 
 export function Home () {
+  // ::after animation gives problems if buttonDiv has button-animation class
+  // so after the button appears this delete the class
+  useEffect(() => {
+    setTimeout(() => {
+      const buttonContainer = document.querySelector('.home__buttonContainer')
+      buttonContainer.classList.remove('home__button-animation')
+    }, 3200)
+  }, [])
+
   return (
     <section className='home' id='home'>
       <div className='home__titleDiv'>
-        <h1>Santiago Toledano</h1>
-        <h2>Full Stack Web Developer</h2>
+        <h1 className='home__h1-animation'>Santiago Toledano</h1>
+        <h2 className='home__h2-animation'>Full Stack Web Developer</h2>
       </div>
-      <div className='home__buttonContainer'>
+      <div className='home__buttonContainer home__button-animation'>
         <ExpandButton reff='#aboutMe' />
       </div>
     </section>
