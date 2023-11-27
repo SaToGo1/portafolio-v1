49a636b3b0fcc2db228c25c08bdff4c22ccd3ed0
SaToGo1 <stoledano97@gmail.com>
2023-11-27 14:14:37 +0100

    improve mobile style
    content  section
    - Change the image
    - Content will now have a gradient wirh transparency.
    
    footer
    - Change the font size for mobile
    
    home
    - Expandbutton moved upward in mobile so it doesn't stays under Navigation Bar.
    
    Fix a bug with the expand button in projects.

--- src/layout/5-footer/footer.jsx
+++ src/layout/5-footer/footer.jsx
@@ -3,8 +3,8 @@ import './footer.css'
 export function Footer () {
   return (
     <footer className='footer'>
-      <p>Designed & coded by <span className='color-red'>Santiago Toledano</span> | Still under development</p>
-      <p><a href='https://github.com/SaToGo1/portfolio-v1'>Code of the Portfolio</a></p>
+      <p className='footer__p'>Designed & coded by <span className='color-red'>Santiago Toledano</span> | Still under development</p>
+      <p className='footer__p'><a href='https://github.com/SaToGo1/portfolio-v1'>Code of the Portfolio</a></p>
     </footer>
   )
 }
